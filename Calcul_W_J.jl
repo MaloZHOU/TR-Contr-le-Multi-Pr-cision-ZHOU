3ec97efac542bc7f0c6a926a13a5e4da542fd6d9
MaloZHOU <lechuan.zhou@etu.minesparis.psl.eu>
2025-01-20 06:55:50 +0100

    Add files via upload

--- Calcul_W_J.jl
+++ Calcul_W_J.jl
@@ -34,7 +34,7 @@ function Calcul_H_model(goddard_model,name_model,nh=100)
     y = res.multipliers
     n = NLPModels.get_nvar(nlp)
     m = NLPModels.get_ncon(nlp)
-
+    
     nnzh = NLPModels.get_nnzh(nlp)
     Wi, Wj = NLPModels.hess_structure(nlp)
     Wz = zeros(nnzh)
@@ -56,17 +56,92 @@ function Calcul_H_model(goddard_model,name_model,nh=100)
 end
 
 function Calcul_eigvals_models(models,names,nh = 100)
-    Maxs = zeros(length(models))
-    Mins = zeros(length(models))
-    
+    Xs = []
+    Ys = []
     for i in range(1,length(models))
         H=Calcul_H_model(models[i],names[i],nh)
         Eigs = eigvals(H)
-        Maxs[i] = maximum(Eigs)
-        Mins[i] = minimum(Eigs)
+        append!(Xs,Eigs)
+        append!(Ys,[i for n in range(1,length(Eigs))])
     end
-    Plots.bar(Maxs, fillto =Mins,xticks = (1:length(names),names),label = false)
-    Plots.savefig("Variance in Eig, nh= $nh.png")
+    Plots.scatter(Ys,Xs,xticks = (1:length(names),names),label = false)
+    Plots.savefig("Variance in Eig,scatter, nh= $nh.png")
 end
 
-Calcul_eigvals_models([rocket_model,rocket_model_hersim,rocket_model_euler_exp,rocket_model_euler_imp],["Trapezoidal","Hermite-Simpson","Euler-Explicite","Euler-Implicite"])
+function Calcul_eigvals_euler(nh = 100,tols = [1e-6])
+    ###Generate the additional lines in J_act
+    ###Paramaters
+    model = rocket_model(nh)
+    nlp = MathOptNLPModel(model)
+    res = ipopt(nlp)
+    
+    ind_h = 1:101
+    ind_v = 102:202
+    ind_m = 203:303
+    ind_T = 304:404
+    Lim_l_h = 1.0
+    Lim_l_v = 0.0
+    Lim_l_m = 0.6
+    Lim_l_T = 0.0
+    Lim_u_m = 1.0
+    Lim_u_T = 3.5*1.0*1.0
+    
+    I_l = zeros(4,405)
+    I_u = zeros(2,405)
+    
+    sol_h = res.solution[ind_h]
+    sol_v = res.solution[ind_v]
+    sol_m = res.solution[ind_m]
+    sol_T = res.solution[ind_T]
+    
+    p = Plots.plot(sol_h,label = "H")
+    Plots.plot!(sol_v,label = "v")
+    Plots.plot!(sol_m,label = "m")
+    Plots.plot!(sol_T,label  = "T")
+    Plots.display(p)
+
+    ###Same ways to generate J
+    x = res.solution
+    y = res.multipliers
+    n = NLPModels.get_nvar(nlp)
+    m = NLPModels.get_ncon(nlp)
+    nnzh = NLPModels.get_nnzh(nlp)
+    Wi, Wj = NLPModels.hess_structure(nlp)
+    Wz = zeros(nnzh)
+    NLPModels.hess_coord!(nlp, x, y, Wz)
+    W = sparse(Wi, Wj, Wz, n, n)
+    nnzj = NLPModels.get_nnzj(nlp)
+    Ji, Jj = NLPModels.jac_structure(nlp)
+    Jz = zeros(nnzj)
+    NLPModels.jac_coord!(nlp, x, Jz)
+    J = sparse(Ji, Jj, Jz, m, n)
+    J1 = Matrix(J)
+
+    ###Add J_iu and calculate eigvalues and plot
+    Xs = []
+    Ys = []
+    for i in range(1,length(tols))
+        tol = tols[i]
+        I_l[1,ind_h] = -(abs.(sol_h .- Lim_l_h).<= tol) .+ 0 
+        I_l[2,ind_v] = -(abs.(sol_v .- Lim_l_v).<= tol) .+ 0 
+        I_l[3,ind_m] = -(abs.(sol_m .- Lim_l_m).<= tol) .+ 0 
+        I_l[4,ind_T] = -(abs.(sol_T .- Lim_l_T).<= tol) .+ 0 
+        I_u[1,ind_m] = (abs.(sol_m .- Lim_u_m).<= tol) .+ 0
+        I_u[2,ind_T] = (abs.(sol_T .- Lim_u_T).<= tol) .+ 0    
+        I_lu = vcat(I_l,I_u)
+
+        J_act = vcat(J1,I_lu)
+        println("Added $(size(J_act)[1]-size(J1)[1]) lines to formal Jacobian as real inequal constrains")
+        Z = nullspace(J_act)
+        H = Z'*Symmetric(W, :L)*Z
+        present_Heatmap(H,"H_act of euler explicit, tol = $(tol)")
+        Eigs = eigvals(H)
+        append!(Xs,Eigs)
+        append!(Ys,[i for n in range(1,length(Eigs))])
+    end
+    Plots.scatter(Ys,Xs,xticks = (1:length(tols),tols),label = false,xlabel = "Tolerance",ylabel = "Eigvals",title = "Egivalues of Matrix H, by explicit Euler ")
+    Plots.savefig("Variance in Eig with actual J, nh= $nh.png")
+end
+
+Calcul_eigvals_euler(100,[1e-8,1e-6,1e-5,1e-4,1e-2])
+# Calcul_eigvals_models([rocket_model,rocket_model_hersim,rocket_model_euler_exp,rocket_model_euler_imp],["Trapezoidal","Hermite-Simpson","Euler-Explicite","Euler-Implicite"])
--- goddard.jl
+++ goddard.jl
@@ -1,3 +1,4 @@
+
 using JuMP
 using Ipopt
 #using COPSBenchmark
@@ -43,8 +44,8 @@ function rocket_model(nh)
     end)
 
     #Set Objective
-    #@objective(model, Max, h[nh])
-    @objective(model, Max, h[nh] + 1e-8 * sum(T[i]^2 for i in 0:nh))
+    @objective(model, Max, h[nh])
+    #@objective(model, Max, h[nh] + 1e-5 * sum(T[i]^2 for i in 0:nh))
     
     # Dynamics
     @constraints(model, begin
@@ -52,6 +53,72 @@ function rocket_model(nh)
         con_dv[i=1:nh], v[i] == v[i-1] + 0.5 * step * (dv[i] + dv[i-1])
         con_dm[i=1:nh], m[i] == m[i-1] + 0.5 * step * (dm[i] + dm[i-1])
     end)
+
+    # Boundary constraints
+    @constraints(model, begin
+        h_ic, h[0] == h_0
+        v_ic, v[0] == v_0
+        m_ic, m[0] == m_0
+        m_fc, m[nh] == m_f
+    end)
+
+    return model
+end
+
+function rocket_model_act(nh)
+    h_0 = 1.0
+    v_0 = 0.0
+    m_0 = 1.0
+    g_0 = 1.0
+    T_c = 3.5
+    h_c = 500.0
+    v_c = 620.0
+    m_c = 0.6
+
+    c = 0.5*sqrt(g_0 * h_0)
+    m_f = m_c * m_0
+    D_c = 0.5 * v_c * (m_0 / g_0)
+    T_max = T_c * m_0 * g_0
+
+    model = Model()
+
+    @variables(model, begin
+        h[i=0:nh],          (start=1.0)
+        v[i=0:nh],   (start=i/nh*(1.0 - i/nh))
+        m[i=0:nh],   (start=(m_f - m_0)*(i/nh) + m_0)
+        T[i=0:nh], (start=T_max/2.0)
+        0.0 <= step,               (start=1/nh)
+    end)
+
+    @expressions(model, begin
+        D[i=0:nh],  D_c*v[i]^2*exp(-h_c*(h[i] - h_0))/h_0
+        g[i=0:nh],  g_0 * (h_0 / h[i])^2
+        dh[i=0:nh], v[i]
+        dv[i=0:nh], (T[i] - D[i] - m[i]*g[i]) / m[i]
+        dm[i=0:nh], -T[i]/c
+    end)
+
+    #Set Objective
+    @objective(model, Max, h[nh])
+    #@objective(model, Max, h[nh] + 1e-5 * sum(T[i]^2 for i in 0:nh))
+    
+    # Dynamics
+    @constraints(model, begin
+        con_dh[i=1:nh], h[i] == h[i-1] + 0.5 * step * (dh[i] + dh[i-1])
+        con_dv[i=1:nh], v[i] == v[i-1] + 0.5 * step * (dv[i] + dv[i-1])
+        con_dm[i=1:nh], m[i] == m[i-1] + 0.5 * step * (dm[i] + dm[i-1])
+    end)
+
+    
+    @constraints(model, begin
+        ineq_h_d[i=0:nh], h[i] >= 1 
+        ineq_v_d[i=0:nh], v[i] >= 0.0 
+        ineq_m_d[i=0:nh], m[i] >= m_f 
+        ineq_T_d[i=0:nh], T[i] >= 0.0
+        ineq_m_u[i=0:nh], m[i] <= m_0 
+        ineq_T_u[i=0:nh], T[i] <= T_max
+    end)
+
     # Boundary constraints
     @constraints(model, begin
         h_ic, h[0] == h_0
@@ -70,7 +137,7 @@ function Generate_thrust(nhs=nhs)
     for i in range(1,length(nhs))
         println(nhs[i])
         nh = nhs[i]
-        model = rocket_model(nh)
+        model = rocket_model_act(nh)
         JuMP.set_optimizer(model, Ipopt.Optimizer)
         JuMP.set_attribute(model,"tol",1e-8)
         JuMP.set_attribute(model, "hsllib", HSL_jll.libhsl_path)
@@ -86,6 +153,6 @@ function Generate_thrust(nhs=nhs)
 end
 
 
-# P = Generate_thrust([1000,5000,10000,12000])
+# P = Generate_thrust([50,100,500,1000,2000])
 # Plots.display(P)
 # Plots.savefig("Nouvel_obj_trapèze_tol=1e-8.png")
