f9e1631098b935eede4f83110484db3e2fb67ce6
MaloZHOU <lechuan.zhou@etu.minesparis.psl.eu>
2025-01-21 14:15:31 +0100

    Add files via upload

--- Calcul_W_J.jl
+++ Calcul_W_J.jl
@@ -68,38 +68,13 @@ function Calcul_eigvals_models(models,names,nh = 100)
     Plots.savefig("Variance in Eig,scatter, nh= $nh.png")
 end
 
-function Calcul_eigvals_euler(nh = 100,tols = [1e-6])
+function Calcul_eigvals_act(nh = 100,tols = [1e-6])
     ###Generate the additional lines in J_act
     ###Paramaters
-    model = rocket_model(nh)
+    model = rocket_model_hersim(nh)
     nlp = MathOptNLPModel(model)
     res = ipopt(nlp)
     
-    ind_h = 1:101
-    ind_v = 102:202
-    ind_m = 203:303
-    ind_T = 304:404
-    Lim_l_h = 1.0
-    Lim_l_v = 0.0
-    Lim_l_m = 0.6
-    Lim_l_T = 0.0
-    Lim_u_m = 1.0
-    Lim_u_T = 3.5*1.0*1.0
-    
-    I_l = zeros(4,405)
-    I_u = zeros(2,405)
-    
-    sol_h = res.solution[ind_h]
-    sol_v = res.solution[ind_v]
-    sol_m = res.solution[ind_m]
-    sol_T = res.solution[ind_T]
-    
-    p = Plots.plot(sol_h,label = "H")
-    Plots.plot!(sol_v,label = "v")
-    Plots.plot!(sol_m,label = "m")
-    Plots.plot!(sol_T,label  = "T")
-    Plots.display(p)
-
     ###Same ways to generate J
     x = res.solution
     y = res.multipliers
@@ -115,33 +90,28 @@ function Calcul_eigvals_euler(nh = 100,tols = [1e-6])
     Jz = zeros(nnzj)
     NLPModels.jac_coord!(nlp, x, Jz)
     J = sparse(Ji, Jj, Jz, m, n)
-    J1 = Matrix(J)
-
     ###Add J_iu and calculate eigvalues and plot
     Xs = []
     Ys = []
     for i in range(1,length(tols))
         tol = tols[i]
-        I_l[1,ind_h] = -(abs.(sol_h .- Lim_l_h).<= tol) .+ 0 
-        I_l[2,ind_v] = -(abs.(sol_v .- Lim_l_v).<= tol) .+ 0 
-        I_l[3,ind_m] = -(abs.(sol_m .- Lim_l_m).<= tol) .+ 0 
-        I_l[4,ind_T] = -(abs.(sol_T .- Lim_l_T).<= tol) .+ 0 
-        I_u[1,ind_m] = (abs.(sol_m .- Lim_u_m).<= tol) .+ 0
-        I_u[2,ind_T] = (abs.(sol_T .- Lim_u_T).<= tol) .+ 0    
-        I_lu = vcat(I_l,I_u)
-
-        J_act = vcat(J1,I_lu)
-        println("Added $(size(J_act)[1]-size(J1)[1]) lines to formal Jacobian as real inequal constrains")
-        Z = nullspace(J_act)
+        Al = findall(res.multipliers_L .> tol)
+        Au = findall(res.multipliers_U .> tol)
+    
+        nl = length(Al)
+        nu = length(Au)
+        Jact = sparse([Ji; m+1:m+nl; m+nl+1:m+nl+nu], [Jj; Al; Au], [Jz; ones(nl); ones(nu)], m+nl+nu, n)
+        
+        Z = nullspace(Matrix(Jact))
         H = Z'*Symmetric(W, :L)*Z
-        present_Heatmap(H,"H_act of euler explicit, tol = $(tol)")
+        present_Heatmap(H,"H_act/H_act of Herm-Sim, tol = $(tol)")
         Eigs = eigvals(H)
         append!(Xs,Eigs)
         append!(Ys,[i for n in range(1,length(Eigs))])
     end
-    Plots.scatter(Ys,Xs,xticks = (1:length(tols),tols),label = false,xlabel = "Tolerance",ylabel = "Eigvals",title = "Egivalues of Matrix H, by explicit Euler ")
-    Plots.savefig("Variance in Eig with actual J, nh= $nh.png")
+    Plots.scatter(Ys,Xs,xticks = (1:length(tols),tols),label = false,xlabel = "Tolerance",ylabel = "Eigvals",title = "Egivalues of Matrix H, by Herm-Sim")
+    Plots.savefig("H_act/Variance in Eig with actual J, Herm-Sim, nh= $nh.png")
 end
 
-Calcul_eigvals_euler(100,[1e-8,1e-6,1e-5,1e-4,1e-2])
+Calcul_eigvals_act(100,[1e-8,1e-6,1e-5,1e-4,1e-2])
 # Calcul_eigvals_models([rocket_model,rocket_model_hersim,rocket_model_euler_exp,rocket_model_euler_imp],["Trapezoidal","Hermite-Simpson","Euler-Explicite","Euler-Implicite"])
